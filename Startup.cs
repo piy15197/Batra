9533177733b269cff88e582cd5c33647db7f0d96
piy15197 <piy15197@gmail.com>
2021-10-10 19:01:40 +0530

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Batra.Startup))]
+namespace Batra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
